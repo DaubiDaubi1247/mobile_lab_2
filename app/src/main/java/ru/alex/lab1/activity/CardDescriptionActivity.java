32880cf976f824786d70e20a8b2c011a6ae8831b
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-03-17 20:42:02 +0300

    set MonsterDto to layout

--- app/src/main/java/ru/alex/lab1/activity/CardDescriptionActivity.java
+++ app/src/main/java/ru/alex/lab1/activity/CardDescriptionActivity.java
@@ -3,22 +3,43 @@ package ru.alex.lab1.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ru.alex.lab1.R;
+import ru.alex.lab1.db.DataBase;
+import ru.alex.lab1.dto.MonsterDto;
+import ru.alex.lab1.pojo.Monster;
 
 public class CardDescriptionActivity extends AppCompatActivity {
 
-    private int cardId;
-
+    private MonsterDto monsterDto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_description);
-        cardId = getIntent().getIntExtra("id", 1);
 
-        TextView textView = findViewById(R.id.card_description_description);
-        textView.setText("" + cardId);
+        int cardId = getIntent().getIntExtra("id", 1);
+        monsterDto = getMonsterDtoFromServer(cardId);
 
+        setMonsterToLayout();
+    }
+
+    private void setMonsterToLayout() {
+        ImageView imageView = findViewById(R.id.card_description_img);
+        imageView.setImageResource(monsterDto.getImgRes());
+
+        TextView textView = findViewById(R.id.card_description_name);
+        textView.setText(monsterDto.getName());
+
+        textView = findViewById(R.id.card_description_quote);
+        textView.setText(monsterDto.getQuote());
+
+        textView = findViewById(R.id.card_description_description);
+        textView.setText(monsterDto.getDescription());
+    }
+
+    private MonsterDto getMonsterDtoFromServer(int id) {
+        return DataBase.getInstance().getMonsterById(id);
     }
 }
