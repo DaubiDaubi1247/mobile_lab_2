d73844471eb548311b2f580e272c6abe401ad39a
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-04-10 13:02:45 +0300

    create updateAllMethod and create AppDataBase

--- app/src/main/java/ru/alex/lab1/dao/MonsterClassDao.java
+++ app/src/main/java/ru/alex/lab1/dao/MonsterClassDao.java
@@ -3,9 +3,11 @@ package ru.alex.lab1.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
+import ru.alex.lab1.dto.MonsterClassDto;
 import ru.alex.lab1.entity.MonsterClass;
 
 @Dao
@@ -15,4 +17,7 @@ public interface MonsterClassDao {
 
     @Insert
     void insertAll(List<MonsterClass> monsterClassList);
+
+    @Update(entity = MonsterClass.class)
+    void updateAll(List<MonsterClassDto> monsterClassDtoList);
 }
---
+++ app/src/main/java/ru/alex/lab1/db/AppDataBase.java
@@ -0,0 +1,6 @@
+package ru.alex.lab1.db;
+
+import androidx.room.RoomDatabase;
+
+abstract class AppDataBase extends RoomDatabase {
+}
